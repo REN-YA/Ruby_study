da8a2859757ae199c329ed05b68ec72c543c3408
REN-YA <re_n_ya@icloud.com>
2022-05-11 07:26:02 +0000

    study

--- ruby.rb
+++ ruby.rb
@@ -1,2 +1,18 @@
-array = (1..40000).to_a
-puts array.select{ |num| num %3 == 0 }.inject(:+)
+def fizz_buzz(number)
+  if number % 15 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number.to_s
+  end
+end
+
+puts "数字を入力してください。"
+
+input = gets.to_i
+
+puts '結果は...'
+puts fizz_buzz(input)
---
+++ study.rb
@@ -0,0 +1,20 @@
+# def fizz_buzz(number)
+  for number in 1..100 do
+    if number % 15 == 0
+      puts "FizzBuzz"
+    elsif number % 3 == 0
+      puts "Fizz"
+    elsif number % 5 == 0
+     puts "Buzz"
+    else
+      puts number
+    end
+  end
+# end
+
+# puts "数字を入力してください。"
+
+# input = gets.to_i
+
+# puts '結果は...'
+puts number
